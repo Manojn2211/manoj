337b9e442c1c51759677c582af12ca30ad59063e
Manojn2211 <144228448+Manojn2211@users.noreply.github.com>
2023-09-09 11:21:46 +0530

    Update dependsonmethod.java

--- QA/src/test/java/TestNG/dependsonmethod.java
+++ QA/src/test/java/TestNG/dependsonmethod.java
@@ -4,7 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-
+manoj chowdary
 public class dependsonmethod {
 	@Test
 	public void login() {
